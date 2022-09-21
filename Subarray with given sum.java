5cf4a16dec53119a10d25ea8b651fbc45cb3d0ac
Akash Tiwari <88731857+tiwari21akash@users.noreply.github.com>
2022-09-21 11:14:23 +0530

    Update Subarray with given sum.java

--- Subarray with given sum.java
+++ Subarray with given sum.java
@@ -1,3 +1,6 @@
+Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S.
+In case of multiple subarrays, return the subarray which comes first on moving from left to right.
+
 class Solution
 {
     //Function to find a continuous sub-array which adds up to a given number.
