651d88b1cd4b28a3bd613f3bb87b4433966d193d
Akash Tiwari <88731857+tiwari21akash@users.noreply.github.com>
2022-09-21 10:54:31 +0530

    Create Subarray with given sum.java

---
+++ Subarray with given sum.java
@@ -0,0 +1,35 @@
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        int sum=0,st=0,en=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[en];
+            if(sum==s){
+                ans.add(st+1);
+                ans.add(en+1);
+                break;
+            }else if(sum>s){
+                i=st++;
+                en=st;
+                sum=0;
+            }else{
+                en++;
+            }
+        }
+        if(ans.size()==0){
+            ans.add(-1);
+        }
+        return ans;
+    }
+}
+
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
